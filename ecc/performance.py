71339d2eed33d7137b8060a21e751292b819872f
Toni Mattis <solaris@live.de>
2012-08-06 16:01:10 +0200

    introducing the Rijndael algorithm

--- ecc/performance.py
+++ ecc/performance.py
@@ -1,5 +1,6 @@
 from Key import Key
 import time
+from rijndael import encrypt, decrypt
 from collections import OrderedDict
 
 def test_generation_perf(n = 100):
@@ -34,6 +35,20 @@ def test_verification_perf(n = 100):
         t = time.time() - t
         results[bits] = t
     return results
+
+def test_cryption(n, method):
+    results = OrderedDict()
+    for ks in (16, 24, 32):
+        for bs in (16, 24, 32):
+            k = 'x' * ks
+            b = 'y' * bs
+            t = time.time()
+            for i in xrange(n):
+                method(k, b)
+            results['Keysize %s / Blocksize %s' % (ks * 8,
+                                                   bs * 8)] = time.time() - t
+    return results
+
             
 def print_dict(title, d):
     print title
@@ -43,8 +58,12 @@ def print_dict(title, d):
     print
 
 n = 100
-print_dict("Key generation", test_generation_perf(n))
-print_dict("Signing", test_signing_perf(n))
-print_dict("Verifying", test_verification_perf(n))
+#print_dict("Key generation", test_generation_perf(n))
+#print_dict("Signing", test_signing_perf(n))
+#print_dict("Verifying", test_verification_perf(n))
+
+n = 1000
+print_dict("AES Encryption", test_cryption(n, encrypt))
+print_dict("AES Decryption", test_cryption(n, decrypt))
 
         
